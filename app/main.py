d7374b16257763c4c538ae57bd75fc69d2cc3e69
Not-Yukii <96383296+Not-Yukii@users.noreply.github.com>
2025-06-06 12:17:32 +0200

    mess

--- app/main.py
+++ app/main.py
@@ -53,7 +53,6 @@ class Token(BaseModel):
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
-    # ⚠️ mettez toujours l'ID en str pour suivre la spec JWT
     to_encode["sub"] = str(to_encode["sub"])
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode["exp"] = expire
